429962812c0a364adb2dc4d5ab3b43fc034ae87e
Adibas03 <adibas03@ymail.com>
2017-07-22 13:00:40 +0100

    Revert "Trying out new process flow"
    This reverts commit 323ce10b0737c12b361dcefdc7bd2f894b013bb6.

--- contracts/Tree.sol
+++ contracts/Tree.sol
@@ -11,7 +11,6 @@ contract Tree{
     bytes8[3] mtypes = [ bytes8("Node"), bytes8("Section"), bytes8("Index")]; //Increase type to match structure types
     enum ltypes {Node,Section,Index} //Increase type to match structure types
     mapping(bytes32=>bytes32) public parent_child_lookup;
-    mapping(bytes32=>bool) child_type_lookup;
 
     uint parent_max_size = 10; //Max size for all parents equaling Max nodes = parent_max_size^(max_depth-1)
 
@@ -59,13 +58,11 @@ contract Tree{
 
         if(search_up.length>0){
             for(uint i=search_up.length-1;i>0;i--){
-              if(search_up[i] == 0x0)
-              continue;
-              //  if(i==search_up.length-1){
-                    //if(mtypes[i-1] == "Section"){
-                        sector = indexes[search_up[i]].children[section_id];
-                    //}
-              //  }
+                if(i==search_up.length-1){
+                    if(mtypes[i-1] == "Section"){
+                        sector = indexes[search_up[i]].children[search_up[i-1]];
+                    }
+                }
             }
         }
     }
@@ -81,27 +78,17 @@ contract Tree{
           return TreeLib.getNodesBatch(getSection(section_id),last_node_id);
     }
 
-    function generateSection() returns (bytes32 section_id){
-      uint i = 0;
-      bytes32 section_id;
-      while(childExists(sha3(block.difficulty+i))){
-        i++;
-      }
-      return sha3(block.difficulty+i);
-    }
-
     function newIndex(bytes32 index_id)idNotEmpty(index_id){
         indexes[index_id] = TreeLib.newIndex(index_id,parent_max_size);
     }
 
-    function insertSection(bytes32 parent_id) idNotEmpty(section_id){
+    function insertSection(bytes32 section_id, bytes32 parent_id) idNotEmpty(section_id){
+        require(!childExists(section_id)); //Ensure it does not exist
 
         //Create new index, if it does not exist
         if(!indexExists(parent_id)){
             newIndex(parent_id);
         }
-        if(index.last == 0x0 || (index.children[index.last].size+1)>parent_max_size)
-          bytes32 section_id = generateSection();
 
         //Parent is an Index, store as child of index
         TreeLib.Index storage index = indexes[parent_id];
@@ -109,31 +96,12 @@ contract Tree{
         TreeLib.insertSection(index,section_id);
     }
 
-    function insertNode(bytes32 index_id, bytes32 node_id, bytes32 data)idNotEmpty(node_id){
+    function insertNode(bytes32 index_id, bytes32 section_id, bytes32 node_id, bytes32 data)idNotEmpty(node_id){
         //node_id required, atleast one of section_id or index_id required
-        require(index_id != 0x0);
+        require(index_id != 0x0 && section_id != 0x0);
 
         //Function heirachy should be updated base on designed structures
         TreeLib.Index storage index = indexes[index_id];
-
-        if(index.last == 0x0 || (index.children[index.last].size+1)>parent_max_size)
-          insertSection(index_id);
-
-          require(index.children[index.last].size<parent_max_size);
-                //last sector can accomodate new node
-                parent_child_lookup[node_id] =  index.last;
-                TreeLib.insertNode(index.children[index.last],node_id,data);
-        }
-        else{
-            if(!childExists(section_id)){
-                insertSection(section_id, index_id);
-            }
-            TreeLib.Section storage sector =  getSection(section_id);
-            require(sector.size<parent_max_size);
-            parent_child_lookup[node_id] =  sector.id;
-            TreeLib.insertNode(sector,node_id,data);
-        }
-        /*
         if(index.last == section_id){
             require(index.children[index.last].size<parent_max_size);
                 //last sector can accomodate new node
@@ -148,7 +116,7 @@ contract Tree{
             require(sector.size<parent_max_size);
             parent_child_lookup[node_id] =  sector.id;
             TreeLib.insertNode(sector,node_id,data);
-        }*/
+        }
 
 
     }
